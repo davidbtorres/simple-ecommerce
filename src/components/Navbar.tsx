37cac0912b8f24fbdcf8a1b4de0e9b8f32153767
David Torres <davidbenjtorres@gmail.com>
2023-06-30 17:13:44 -0600

    adjusted navbar styling and replaced anchors with react-router links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Navbar() {
     return (
-        <nav className="bg-blue-500 p-4">
+        <nav className="bg-teal-400 p-4">
             <div className="container mx-auto">
                 <div className="flex items-center justify-between">
-                    <div className="text-white font-bold text-lg">My Website</div>
+                    <div className="text-white font-bold text-lg">SHELF</div>
                     <ul className="flex space-x-4">
                         <li>
-                            <a href="/" className="text-white hover:underline">
+                            <Link to="/" className="text-white hover:underline">
                                 Home
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a href="/about" className="text-white hover:underline">
+                            <Link to="/about" className="text-white hover:underline">
                                 About
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a href="/store" className="text-white hover:underline">
+                            <Link to="/store" className="text-white hover:underline">
                                 Store
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
